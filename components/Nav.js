0cec322d33c501521db72df176f69977858a811b
joanjeremiah <81401721+joanjeremiah@users.noreply.github.com>
2021-10-23 05:41:15 +0530

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,50 +1,12 @@
 import Image from 'next/image'
 import logo from '../public/images/logo.png'
 import MenuIcon from './MenuIcon'
-import { useRef } from 'react'
-
-
-// const Nav = () => {
-//     const mobileMenu = useRef();
-//     const displayOrHideMenu = () => {
-//         mobileMenu.current.classList.toggle('display');
-
-//     }
-//     return(
-//        <header id="nav">
-//            <nav className="nav">
-//                <div className="logo-wrapper">
-//                     <Image src={logo} width={70} height={100}  />
-//                </div>
-//                <ul className="uppercase">
-//                     <li className="active"><a href="#slider">Home</a></li>
-//                     <li className="smooth-scroll"><a href="#about">About</a></li>
-//                     <li><a href="#feature">Feature</a></li>
-//                     <li><a href="#screnshot">screenshot</a></li>
-//                     <li><a href="#reviews">Testimonial</a></li>
-//                     <li><a href="#contact">Contact</a></li>
-//                </ul>
-//                <MenuIcon open="true" menu={displayOrHideMenu} />
-//            </nav>
-//            <div className="mobile-menu" ref={mobileMenu}>
-//                 <ul className="uppercase">
-//                     <li className="active"><a href="#slider">Home</a></li>
-//                     <li className="smooth-scroll"><a href="#about">About</a></li>
-//                     <li><a href="#feature">Feature</a></li>
-//                     <li><a href="#screnshot">screenshot</a></li>
-//                     <li><a href="#reviews">Testimonial</a></li>
-//                     <li><a href="#contact">Contact</a></li>
-//                 </ul>
-//             </div>
-//        </header>
-//     );
-// }
 
 import React from 'react'
 
 class Nav extends React.Component {
   listener = null;
-  const mobileMenu = React.createRef();
+  mobileMenu = React.createRef();
   state = {
     nav:false
   }
